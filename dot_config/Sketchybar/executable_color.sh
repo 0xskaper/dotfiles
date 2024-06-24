d8e136b76b1e3313d07778daa60d5f606fe21059
laggedskapari <es.rajatyadav@gmail.com>
2024-06-24 21:01:24 +0530

    Remove .config/Sketchybar/LICENSE.md Remove .config/Sketchybar/README.md Remove .config/Sketchybar/.github/FUNDING.yml Remove .config/Sketchybar/.gitignore Remove .config/Sketchybar/icon.font=JetBrainsMono Nerd Font:Bold:16.0 Remove .config/Sketchybar/color.sh Remove .config/Sketchybar/sketchybarrc Remove .config/Sketchybar/variables.sh Remove .config/Sketchybar/images/Sbar.svg Remove .config/Sketchybar/images/bar_item.svg Remove .config/Sketchybar/images/default.png Remove .config/Sketchybar/images/example.png Remove .config/Sketchybar/items/clock.sh Remove .config/Sketchybar/items/cpu.sh Remove .config/Sketchybar/items/front_app.sh Remove .config/Sketchybar/items/spaces.sh Remove .config/Sketchybar/makefile Remove .config/Sketchybar/plugins/clock.sh Remove .config/Sketchybar/plugins/cpu.sh Remove .config/Sketchybar/plugins/flow.sh Remove .config/Sketchybar/plugins/front_app.sh Remove .config/Sketchybar/plugins/space.sh Remove .config/Sketchybar/src/alias.c Remove .config/Sketchybar/src/alias.h Remove .config/Sketchybar/src/animation.c Remove .config/Sketchybar/src/animation.h Remove .config/Sketchybar/src/app_windows.c Remove .config/Sketchybar/src/app_windows.h Remove .config/Sketchybar/src/background.c Remove .config/Sketchybar/src/background.h Remove .config/Sketchybar/src/bar.c Remove .config/Sketchybar/src/bar.h Remove .config/Sketchybar/src/bar_item.c Remove .config/Sketchybar/src/bar_item.h Remove .config/Sketchybar/src/bar_manager.c Remove .config/Sketchybar/src/bar_manager.h Remove .config/Sketchybar/src/color.c Remove .config/Sketchybar/src/color.h Remove .config/Sketchybar/src/custom_events.c Remove .config/Sketchybar/src/custom_events.h Remove .config/Sketchybar/src/display.c Remove .config/Sketchybar/src/display.h Remove .config/Sketchybar/src/event.c Remove .config/Sketchybar/src/event.h Remove .config/Sketchybar/src/font.c Remove .config/Sketchybar/src/font.h Remove .config/Sketchybar/src/graph.c Remove .config/Sketchybar/src/graph.h Remove .config/Sketchybar/src/group.c Remove .config/Sketchybar/src/group.h Remove .config/Sketchybar/src/hotload.c Remove .config/Sketchybar/src/hotload.h Remove .config/Sketchybar/src/image.c Remove .config/Sketchybar/src/image.h Remove .config/Sketchybar/src/mach.c Remove .config/Sketchybar/src/mach.h Remove .config/Sketchybar/src/media.h Remove .config/Sketchybar/src/media.m Remove .config/Sketchybar/src/message.c Remove .config/Sketchybar/src/message.h Remove .config/Sketchybar/src/misc/defines.h Remove .config/Sketchybar/src/misc/env_vars.h Remove .config/Sketchybar/src/misc/help.h Remove .config/Sketchybar/src/misc/helpers.h Remove .config/Sketchybar/src/mouse.c Remove .config/Sketchybar/src/mouse.h Remove .config/Sketchybar/src/popup.c Remove .config/Sketchybar/src/popup.h Remove .config/Sketchybar/src/power.c Remove .config/Sketchybar/src/power.h Remove .config/Sketchybar/src/shadow.c Remove .config/Sketchybar/src/shadow.h Remove .config/Sketchybar/src/sketchybar.c Remove .config/Sketchybar/src/slider.c Remove .config/Sketchybar/src/slider.h Remove .config/Sketchybar/src/text.c Remove .config/Sketchybar/src/text.h Remove .config/Sketchybar/src/volume.c Remove .config/Sketchybar/src/volume.h Remove .config/Sketchybar/src/wifi.h Remove .config/Sketchybar/src/wifi.m Remove .config/Sketchybar/src/window.c Remove .config/Sketchybar/src/window.h Remove .config/Sketchybar/src/workspace.h Remove .config/Sketchybar/src/workspace.m

--- dot_config/Sketchybar/executable_color.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export WHITE=0xffffffff
-
-# --laggedskapri scheme--
-export BAR_COLOR=0x00ffffff
--- dot_config/Sketchybar/plugins/executable_clock.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-LABEL=$(date '+%A, %d %B`%y 󰄉 %l:%M:%S %p')
-sketchybar --set "$NAME" label="$LABEL"
