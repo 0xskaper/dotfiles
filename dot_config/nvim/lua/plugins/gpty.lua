748a1119fede86d1e02a91bcd6ad5862234ef91f
laggedskapari <es.rajatyadav@gmail.com>
2024-06-25 01:46:24 +0530

    Update .config/nvim/lua/plugins/gpty.lua

--- dot_config/nvim/lua/plugins/gpty.lua
+++ dot_config/nvim/lua/plugins/gpty.lua
@@ -1,13 +1,17 @@
 return {
   "jackMort/ChatGPT.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("chatgpt").setup()
-    end,
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "folke/trouble.nvim",
-      "nvim-telescope/telescope.nvim"
-    }
+  event = "VeryLazy",
+  config = function()
+    require("chatgpt").setup({
+      openai_params = {
+        model = "gpt-3.5-turbo",
+      },
+    })
+  end,
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "folke/trouble.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
 }
