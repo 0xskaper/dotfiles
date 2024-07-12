e1d6349b5c661544f1b7feb2e02843f3f66e0d1a
laggedskapari <es.rajatyadav@gmail.com>
2024-07-12 14:58:34 +0530

    Update .config/nvim/lua/plugins/neotest.lua

--- dot_config/nvim/lua/plugins/neotest.lua
+++ dot_config/nvim/lua/plugins/neotest.lua
@@ -1,49 +1,51 @@
 return {
-	"nvim-neotest/neotest",
-	dependencies = {
-		"nvim-neotest/nvim-nio",
-		"nvim-lua/plenary.nvim",
-		"antoinemadec/FixCursorHold.nvim",
-		"nvim-treesitter/nvim-treesitter",
-		"fredrikaverpil/neotest-golang", -- Installation
-	},
-	config = function()
-		vim.keymap.set("n", "<leader>tn", ":lua require('neotest').run.run() <CR>", {})
-		vim.keymap.set("n", "<leader>mo", ":lua require('neotest').output.open({ enter = true }) <CR>", {})
-		require("neotest").setup({
-			adapters = {
-				require("neotest-golang"), -- Registration
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"nvim-lua/plenary.nvim",
+			"antoinemadec/FixCursorHold.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"fredrikaverpil/neotest-golang", -- Installation
+		},
+		config = function()
+			vim.keymap.set("n", "<leader>tn", ":lua require('neotest').run.run() <CR>", {})
+			vim.keymap.set("n", "<leader>mo", ":lua require('neotest').output.open({ enter = true }) <CR>", {})
+			require("neotest").setup({
+				adapters = {
+					require("neotest-golang"), -- Registration
+				},
+			})
+		end,
+		keys = {
+			{
+				"<leader>td",
+				function()
+					require("neotest").run.run()
+				end,
+				desc = "Debug nearest test",
 			},
-		})
-	end,
-	keys = {
-		{
-			"<leader>td",
-			function()
-				require("neotest").run.run()
-			end,
-			desc = "Debug nearest test",
-		},
-		{
-			"<leader>ts",
-			function()
-				require("neotest").run.stop()
-			end,
-			desc = "Debug nearest test",
-		},
-		{
-			"<leader>ta",
-			function()
-				require("neotest").run.attach()
-			end,
-			desc = "Debug nearest test",
-		},
-		{
-			"<leader>tn",
-			function()
-				require("neotest").run.run(vim.expand("%"))
-			end,
-			desc = "Debug nearest test",
+			{
+				"<leader>ts",
+				function()
+					require("neotest").run.stop()
+				end,
+				desc = "Debug nearest test",
+			},
+			{
+				"<leader>ta",
+				function()
+					require("neotest").run.attach()
+				end,
+				desc = "Debug nearest test",
+			},
+			{
+				"<leader>tn",
+				function()
+					require("neotest").run.run(vim.expand("%"))
+				end,
+				desc = "Debug nearest test",
+			},
 		},
 	},
 }
