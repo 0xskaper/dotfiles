441c94794251b94c6510921d8e6642c893e6c5a8
laggedskapari <es.rajatyadav@gmail.com>
2024-07-12 15:19:29 +0530

    Update .config/nvim/lua/plugins/neotest.lua

--- dot_config/nvim/lua/plugins/neotest.lua
+++ dot_config/nvim/lua/plugins/neotest.lua
@@ -9,8 +9,6 @@ return {
 			"fredrikaverpil/neotest-golang", -- Installation
 		},
 		config = function()
-			vim.keymap.set("n", "<leader>tn", ":lua require('neotest').run.run() <CR>", {})
-			vim.keymap.set("n", "<leader>mo", ":lua require('neotest').output.open({ enter = true }) <CR>", {})
 			require("neotest").setup({
 				adapters = {
 					require("neotest-golang"), -- Registration
