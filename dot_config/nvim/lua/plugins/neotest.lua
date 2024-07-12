70ee2756245ea31742514e20a99502bfe79c0ff5
laggedskapari <es.rajatyadav@gmail.com>
2024-07-12 14:27:55 +0530

    Update .config/nvim/lua/plugins/neotest.lua

--- dot_config/nvim/lua/plugins/neotest.lua
+++ dot_config/nvim/lua/plugins/neotest.lua
@@ -1,20 +1,28 @@
 return {
-  "nvim-neotest/neotest",
-  dependencies = {
-    "nvim-neotest/nvim-nio",
-    "nvim-lua/plenary.nvim",
-    "antoinemadec/FixCursorHold.nvim",
-    "nvim-treesitter/nvim-treesitter",
-    "fredrikaverpil/neotest-golang", -- Installation
-  },
-  config = function()
-    vim.keymap.set("n", "<leader>tn", ":lua require('neotest').run.run() <CR>", {})
-    vim.keymap.set("n", "<leader>mo", ":lua require('neotest').output.open({ enter = true }) <CR>", {})
-    require("neotest").setup({
-      adapters = {
-        require("neotest-golang"), -- Registration
-      },
-    })
-  end,
+	"nvim-neotest/neotest",
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+		"nvim-lua/plenary.nvim",
+		"antoinemadec/FixCursorHold.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		"fredrikaverpil/neotest-golang", -- Installation
+	},
+	config = function()
+		vim.keymap.set("n", "<leader>tn", ":lua require('neotest').run.run() <CR>", {})
+		vim.keymap.set("n", "<leader>mo", ":lua require('neotest').output.open({ enter = true }) <CR>", {})
+		require("neotest").setup({
+			adapters = {
+				require("neotest-golang"), -- Registration
+			},
+		})
+	end,
+	keys = {
+		{
+			"<leader>td",
+			function()
+				require("neotest").run.run({ suite = false, strategy = "dap" })
+			end,
+			desc = "Debug nearest test",
+		},
+	},
 }
-
