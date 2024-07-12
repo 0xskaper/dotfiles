ae3ec22bd1d39371116c835b54eeecfc625e0616
laggedskapari <es.rajatyadav@gmail.com>
2024-07-12 16:37:25 +0530

    Update .config/nvim/lua/plugins/neotest.lua

--- dot_config/nvim/lua/plugins/neotest.lua
+++ dot_config/nvim/lua/plugins/neotest.lua
@@ -1,58 +1,55 @@
 return {
-	{
-		"nvim-neotest/neotest",
-		dependencies = {
-			"nvim-neotest/nvim-nio",
-			"nvim-lua/plenary.nvim",
-			"antoinemadec/FixCursorHold.nvim",
-			"nvim-treesitter/nvim-treesitter",
-			"fredrikaverpil/neotest-golang",
-      "neotest/neotest-python"-- Installation
+	"nvim-neotest/neotest",
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+		"nvim-lua/plenary.nvim",
+		"antoinemadec/FixCursorHold.nvim",
+		"nvim-treesitter/nvim-treesitter",
+		"fredrikaverpil/neotest-golang",
+	},
+	config = function()
+		require("neotest").setup({
+			adapters = {
+				require("neotest-golang"), -- Registration
+				require("neotest-python"),
+			},
+		})
+	end,
+	keys = {
+		{
+			"<leader>td",
+			function()
+				require("neotest").run.run()
+			end,
+			desc = "Debug nearest test",
 		},
-		config = function()
-			require("neotest").setup({
-				adapters = {
-					require("neotest-golang"), -- Registration
-					require("neotest-python"),
-				},
-			})
-		end,
-		keys = {
-			{
-				"<leader>td",
-				function()
-					require("neotest").run.run()
-				end,
-				desc = "Debug nearest test",
-			},
-			{
-				"<leader>ts",
-				function()
-					require("neotest").run.stop()
-				end,
-				desc = "Debug nearest test",
-			},
-			{
-				"<leader>ta",
-				function()
-					require("neotest").run.attach()
-				end,
-				desc = "Debug nearest test",
-			},
-			{
-				"<leader>tn",
-				function()
-					require("neotest").run.run(vim.expand("%"))
-				end,
-				desc = "Debug nearest test",
-			},
-			{
-				"<leader>lf",
-				function()
-					require("neotest.logging"):get_filename()
-				end,
-				desc = "Debug nearest test",
-			},
+		{
+			"<leader>ts",
+			function()
+				require("neotest").run.stop()
+			end,
+			desc = "Debug nearest test",
+		},
+		{
+			"<leader>ta",
+			function()
+				require("neotest").run.attach()
+			end,
+			desc = "Debug nearest test",
+		},
+		{
+			"<leader>tn",
+			function()
+				require("neotest").run.run(vim.expand("%"))
+			end,
+			desc = "Debug nearest test",
+		},
+		{
+			"<leader>lf",
+			function()
+				require("neotest.logging"):get_filename()
+			end,
+			desc = "Debug nearest test",
 		},
 	},
 }
