8b3a50a59827b9ec4f9ef7df66f82b553149d0b0
laggedskapari <es.rajatyadav@gmail.com>
2024-07-26 13:27:25 +0530

    Update .config/nvim/lua/plugins/nvim-dap.lua

--- dot_config/nvim/lua/plugins/nvim-dap.lua
+++ dot_config/nvim/lua/plugins/nvim-dap.lua
@@ -1,7 +1,12 @@
 return {
   "mfussenegger/nvim-dap",
+  dependecies = {
+    "leoluz/nvim-dap-go",
+  },
   config = function()
     local dap = require("dap")
+
+    require('dap-go').setup()
 
     vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
     vim.keymap.set("n", "<Leader>dc", dap.continue, {})
