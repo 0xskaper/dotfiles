631d6202c34fdc419d597db41dc9121ea3b93a27
laggedskapari <es.rajatyadav@gmail.com>
2024-07-26 13:31:02 +0530

    Remove .config/nvim/lua/plugins/nvim-dap-ui.lua Update .config/nvim/lua/plugins/nvim-dap.lua

--- dot_config/nvim/lua/plugins/nvim-dap.lua
+++ dot_config/nvim/lua/plugins/nvim-dap.lua
@@ -1,14 +1,27 @@
 return {
-  "mfussenegger/nvim-dap",
-  dependecies = {
-    "leoluz/nvim-dap-go",
-  },
-  config = function()
-    local dap = require("dap")
+	"mfussenegger/nvim-dap",
+	dependecies = {
+		"leoluz/nvim-dap-go",
+		"rcarriga/nvim-dap-ui",
+	},
+	config = function()
+		local dap = require("dap")
+		local dapui = require("dapui")
 
-    require('dap-go').setup()
+		require("dap-go").setup()
+    require("dapui").setup()
 
-    vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
-    vim.keymap.set("n", "<Leader>dc", dap.continue, {})
-  end,
+		dap.listeners.after.event_initialized["dapui_config"] = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated["dapui_config"] = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited["dapui_config"] = function()
+			dapui.close()
+		end
+
+		vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
+		vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+	end,
 }
