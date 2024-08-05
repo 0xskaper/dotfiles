f88469a92f98bed6ccd448e02016dbde6177f5a2
laggedskapari <es.rajatyadav@gmail.com>
2024-08-05 07:23:30 +0530

    Update .config/tmux/plugins/tokyo-night-tmux/src/git-status.sh Update .config/tmux/plugins/tokyo-night-tmux/src/music-tmux-statusbar.sh Update .config/tmux/plugins/tokyo-night-tmux/src/wb-git-status.sh

--- dot_config/tmux/plugins/tokyo-night-tmux/src/executable_git-status.sh
+++ dot_config/tmux/plugins/tokyo-night-tmux/src/executable_git-status.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd $1
-RESET="#[fg=brightwhite,bg=#0c0e15,nobold,noitalics,nounderscore,nodim]"
+RESET="#[fg=brightwhite,bg=#1d2021,nobold,noitalics,nounderscore,nodim]"
 BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
 STATUS=$(git status --porcelain 2>/dev/null| egrep "^(M| M)" | wc -l)
 
@@ -16,21 +16,21 @@ if test "$STATUS" != "0"; then
 fi
 
 if [[ $CHANGED_COUNT > 0 ]]; then
-  STATUS_CHANGED="#[fg=#e0af68,bg=#0c0e15,bold] ${CHANGED_COUNT} "
+  STATUS_CHANGED="#[fg=#e0af68,bg=#1d2021,bold] ${CHANGED_COUNT} "
 fi
 
 if [[ $INSERTIONS_COUNT > 0 ]]; then
-  STATUS_INSERTIONS="#[fg=#44dfaf,bg=#0c0e15,bold] ${INSERTIONS_COUNT} "
+  STATUS_INSERTIONS="#[fg=#44dfaf,bg=#1d2021,bold] ${INSERTIONS_COUNT} "
 fi
 
 if [[ $DELETIONS_COUNT > 0 ]]; then
-  STATUS_DELETIONS="#[fg=#f7768e,bg=#0c0e15,bold] ${DELETIONS_COUNT} "
+  STATUS_DELETIONS="#[fg=#f7768e,bg=#1d2021,bold] ${DELETIONS_COUNT} "
 fi
 
 if test "$BRANCH" != ""; then
   if test "$STATUS" = "0"; then
-    echo "#[fg=##1ae0d7,bg=#0c0e15,bold]  $RESET$BRANCH "
+    echo "#[fg=##1ae0d7,bg=#1d2021,bold]  $RESET$BRANCH "
   else
-    echo "#[fg=##1ae0d7,bg=#0c0e15,bold]  $RESET$BRANCH $RESET$STATUS_CHANGED$RESET$STATUS_INSERTIONS$RESET$STATUS_DELETIONS"
+    echo "#[fg=##1ae0d7,bg=#1d2021,bold]  $RESET$BRANCH $RESET$STATUS_CHANGED$RESET$STATUS_INSERTIONS$RESET$STATUS_DELETIONS"
   fi
 fi
