4aec2e4b5d3461e3ea177a9b3a5c6e9267133a8c
laggedskapari <es.rajatyadav@gmail.com>
2024-08-16 07:13:34 +0530

    Remove .config/nvim/.DS_Store Remove .config/nvim/lua/.DS_Store Remove .config/nvim/lua/plugins/.DS_Store Update .config/nvim/lua/plugins/lsp-config.lua

--- dot_config/nvim/lua/plugins/lsp-config.lua
+++ dot_config/nvim/lua/plugins/lsp-config.lua
@@ -25,31 +25,19 @@ return {
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig['sourcekit'].setup({
-        capabilities = capabilities,
-      })
+
+      lspconfig.lua_ls.setup({ capabilities = capabilities })
+      lspconfig["sourcekit"].setup({ capabilities = capabilities })
       lspconfig.dartls.setup({
         capabilities = capabilities,
         cmd = { "dart", "language-server", "--protocol=lsp" },
       })
-      lspconfig.yamlls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.clangd.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.cmake.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.markdown_oxide.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.gopls.setup({
-        capabilities = capabilities,
-      })
+      lspconfig.yamlls.setup({ capabilities = capabilities })
+      lspconfig.clangd.setup({ capabilities = capabilities })
+      lspconfig.cmake.setup({ capabilities = capabilities })
+      lspconfig.markdown_oxide.setup({ capabilities = capabilities })
+      lspconfig.gopls.setup({ capabilities = capabilities })
+      lspconfig.tsserver.setup({ capabilities = capabilities })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
