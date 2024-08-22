cf0561949061d4774df9f14e000f1477ab64b04d
laggedskapari <es.rajatyadav@gmail.com>
2024-08-22 17:43:25 +0530

    Update .config/nvim/lua/plugins/lualine.lua

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -212,7 +212,11 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("lualine").setup(config)
+      require("lualine").setup({
+        options = {
+          theme = "gruvbox_dark",
+        }
+      })
     end,
   },
 }
