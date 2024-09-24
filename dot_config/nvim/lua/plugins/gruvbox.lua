6f1bbb61bd7c6b262907b607ca0041134a7ae5ba
laggeskapari <es.rajatyadav@gmail.com>
2024-09-24 08:12:19 -0400

    Update .config/nvim/lua/plugins/gruvbox.lua

--- dot_config/nvim/lua/plugins/gruvbox.lua
+++ dot_config/nvim/lua/plugins/gruvbox.lua
@@ -1,15 +1,15 @@
 return {
- 	"ellisonleao/gruvbox.nvim",
- 	priority = 1000,
- 	opts = ...,
- 	config = function()
- 		require("gruvbox").setup({
-       terminal_colors = true,
-       contrast = "soft",
-       palette_overrides = {
-         bright_red = "#dd7575",
-       }
- 		})
- 		vim.cmd("colorscheme gruvbox")
- 	end,
- }
+	"ellisonleao/gruvbox.nvim",
+	priority = 1000,
+	opts = ...,
+	config = function()
+		require("gruvbox").setup({
+			terminal_colors = true,
+			contrast = "hard",
+			palette_overrides = {
+				bright_red = "#dd7575",
+			},
+		})
+		vim.cmd("colorscheme gruvbox")
+	end,
+}
