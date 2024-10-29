a3cf107278f34580545abdd075429b5ef1e1576e
laggedskapari <es.rajatyadav@gmail.com>
2024-10-29 08:25:06 -0400

    Update .config/nvim/lua/plugins/live-server.lua

--- dot_config/nvim/lua/plugins/live-server.lua
+++ dot_config/nvim/lua/plugins/live-server.lua
@@ -1,10 +1,13 @@
 return {
-  require("lazy").setup({
-    {
-      "barrett-ruth/live-server.nvim",
-      build = "pnpm add -g live-server",
-      cmd = { "LiveServerStart", "LiveServerStop" },
-      config = true,
-    },
-  }),
+  "barrett-ruth/live-server.nvim",
+  config = function()
+    require("lazy").setup({
+      {
+        "barrett-ruth/live-server.nvim",
+        build = "pnpm add -g live-server",
+        cmd = { "LiveServerStart", "LiveServerStop" },
+        config = true,
+      },
+    })
+  end,
 }
