501876d285e52c576f543e00e115033f9cd41fc3
laggedskapari <es.rajatyadav@gmail.com>
2024-11-05 18:07:44 -0500

    Update .config/nvim/lua/plugins/gruvbox.lua

--- dot_config/nvim/lua/plugins/gruvbox.lua
+++ dot_config/nvim/lua/plugins/gruvbox.lua
@@ -7,7 +7,7 @@ return {
 			terminal_colors = true,
 			palette_overrides = {
         light0 = "#fbf1f7",
-        light1 = "fbf1f7",
+        light1 = "#fbf1f7",
 				bright_red = "#dd7575",
 			},
 		})
