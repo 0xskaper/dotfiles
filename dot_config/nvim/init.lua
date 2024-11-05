87cd273585d39563468a9a696a8aaccc653fbc59
laggedskapari <es.rajatyadav@gmail.com>
2024-11-05 18:05:22 -0500

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -16,6 +16,7 @@ require("vim-options")
 require("lazy").setup("plugins")
 
 vim.o.guifont = "JetBrainsMono Nerd Font:SemiBold:h17"
+vim.o.background = "light"
 
 vim.g.neovide_padding_top = 20
 vim.g.neovide_padding_bottom = 1
