85f9ad80cac1ee8f28c3d293abecc958ba3ed96a
0xskaper <es.rajatyadav@gmail.com>
2024-12-24 19:19:30 -0500

    Update .config/nvim/lua/plugins/lsp-config.lua

--- dot_config/nvim/lua/plugins/lsp-config.lua
+++ dot_config/nvim/lua/plugins/lsp-config.lua
@@ -24,7 +24,6 @@ return {
 		"neovim/nvim-lspconfig",
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			require("java").setup()
 			local lspconfig = require("lspconfig")
 
 			lspconfig.lua_ls.setup({ capabilities = capabilities })
