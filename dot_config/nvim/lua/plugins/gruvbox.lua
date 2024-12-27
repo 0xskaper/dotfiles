8556de965e84223b9bba1422a777dca191f04666
0xskaper <es.rajatyadav@gmail.com>
2024-12-27 15:58:35 -0500

    Update .config/nvim/lua/plugins/gruvbox.lua

--- dot_config/nvim/lua/plugins/gruvbox.lua
+++ dot_config/nvim/lua/plugins/gruvbox.lua
@@ -4,11 +4,9 @@ return {
 	opts = ...,
 	config = function()
 		require("gruvbox").setup({
-			contrast = "hard",
+			contrast = "soft",
 			terminal_colors = true,
 			palette_overrides = {
-				light0_hard = "#f2e5bc",
-				light1 = "#f2e5bc",
 				bright_red = "#dd7575",
 			},
 		})
