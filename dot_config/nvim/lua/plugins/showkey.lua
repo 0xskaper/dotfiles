dc6cc33b5e313ac12aff89df2ac6b126c163d8ef
0xskaper <es.rajatyadav@gmail.com>
2024-12-29 14:45:41 -0500

    Update .config/nvim/lua/plugins/showkey.lua

--- dot_config/nvim/lua/plugins/showkey.lua
+++ dot_config/nvim/lua/plugins/showkey.lua
@@ -1,20 +1,19 @@
 return {
-	"nvchad/showkeys",
-	cmd = "ShowkeysToggle",
-	opts = {
-		timeout = 1,
-		maxkeys = 3,
-		-- bottom-left, bottom-right, bottom-center, top-left, top-right, top-center
-		position = "bottom-center",
-	},
+  "nvchad/showkeys",
+  cmd = "ShowkeysToggle",
+  opts = {
+    timeout = 1,
+    maxkeys = 3,
+    position = "bottom-center",
+  },
 
-	keys = {
-		{
-			"<leader>kt",
-			function()
-				vim.cmd("ShowkeysToggle")
-			end,
-			desc = "Show key presses",
-		},
-	},
+  keys = {
+    {
+      "<leader>kt",
+      function()
+        vim.cmd("ShowkeysToggle")
+      end,
+      desc = "Show key presses",
+    },
+  },
 }
