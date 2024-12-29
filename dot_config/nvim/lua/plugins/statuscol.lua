c3abf5dd867bf11ac1d88e5b199d84ed49c928a1
0xskaper <es.rajatyadav@gmail.com>
2024-12-29 14:44:38 -0500

    Update .config/nvim/lua/plugins/statuscol.lua

--- dot_config/nvim/lua/plugins/statuscol.lua
+++ dot_config/nvim/lua/plugins/statuscol.lua
@@ -3,6 +3,9 @@ return {
   config = function()
     require("statuscol").setup({
       relculright = true,
+      {
+        sign = { namespace = { "diagnostic/signs" }, maxwidth = 2, auto = true },
+      },
     })
   end,
 }
