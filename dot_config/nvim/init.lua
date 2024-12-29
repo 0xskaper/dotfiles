8bf46173664fa7697ea13269afd2ffe8403bc61e
0xskaper <es.rajatyadav@gmail.com>
2024-12-28 23:20:59 -0500

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -35,8 +35,8 @@ vim.g.neovide_position_animation_length = 0.10
 vim.cmd("hi LineNr term=bold gui=bold cterm=bold guifg=#282828")
 vim.cmd("hi LineNrAbove guifg=grey")
 vim.cmd("hi LineNrBelow guifg=grey")
-vim.cmd("hi SignColumn guibg=#fbf1c7")
-vim.cmd("hi Visual guibg=#fbf1c7")
+vim.cmd("hi SignColumn guibg=#f2e5bc")
+vim.cmd("hi Visual guibg=#f2e5bc")
 vim.keymap.set("n", "<leader>cd", ":Copilot disable<CR>")
 vim.keymap.set("n", "<leader>ce", ":Copilot enable<CR>")
 vim.opt.showcmd = true
