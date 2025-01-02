4a52dfdb18653c95e958a4140a0d0e4c53d0ae6d
0xskaper <es.rajatyadav@gmail.com>
2025-01-02 11:47:20 -0500

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,42 +1,54 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
+require("vim-options")
+vim.cmd("set number relativenumber!")
+vim.cmd("hi LineNr term=bold gui=bold cterm=bold guifg=yellow")
+vim.cmd("hi LineNrAbove guifg=grey")
+vim.cmd("hi LineNrBelow guifg=grey")
+vim.cmd("hi SignColumn guibg=#1d2021")
+vim.keymap.set("n", "<leader>cd", ":Copilot disable<CR>")
+vim.keymap.set("n", "<leader>ce", ":Copilot enable<CR>")
+vim.opt.showcmd = true
+vim.opt.cmdheight = 0
+vim.opt.fillchars = { eob = " " }
+vim.loader.enable()
 
 require("lazy").setup({ import = "plugins" }, {
-  install = {
-    missing = true,
-    colorscheme = { "catppuccin", "habamax" },
-  },
-  checker = {
-    enabled = true,
-    notify = false,
-  },
-  change_detection = {
-    enabled = true,
-    notify = false,
-  },
-  ui = {
-    border = "rounded",
-  },
-  performance = {
-    rtp = {
-      disabled_plugins = {
-        "gzip",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
-    },
-  },
+	install = {
+		missing = true,
+		colorscheme = { "catppuccin", "habamax" },
+	},
+	checker = {
+		enabled = true,
+		notify = false,
+	},
+	change_detection = {
+		enabled = true,
+		notify = false,
+	},
+	ui = {
+		border = "rounded",
+	},
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
 })
