6434c4cda4bbedc5cb2fe715be65c7b3ea847d86
0xskaper <es.rajatyadav@gmail.com>
2025-01-08 00:48:31 -0500

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,19 +1,38 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
+
 		"git",
 		"clone",
 		"--filter=blob:none",
 		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
+		"--branch=stable", -- Latest stable release
 		lazypath,
 	})
 end
 
-vim.opt.rtp:prepend(lazypath)
 vim.cmd("set number relativenumber!")
-vim.api.nvim_set_hl(0, "LineNr", { fg = "yellow" })
+vim.opt.rtp:prepend(lazypath)
+require("vim-options")
+
+vim.o.background = "dark"
+
+vim.g.neovide_padding_top = 10
+vim.g.neovide_padding_bottom = 0
+vim.g.neovide_padding_right = 0
+vim.g.neovide_padding_left = 0
+vim.g.neovide_underline_stroke_scale = 0.5
+vim.g.neovide_cursor_antialiasing = true
+vim.g.neovide_cursor_trail_size = 0.2
+vim.g.neovide_refresh_rate = 120
+vim.g.neovide_hide_mouse_when_typing = true
+vim.g.neovide_scroll_animation_length = 0.8
+vim.g.neovide_show_border = true
+vim.g.neovide_fullscreen = false
+vim.g.neovide_cursor_animation_length = 0.05
+vim.g.neovide_position_animation_length = 0.10
+
+vim.cmd("hi LineNr term=bold gui=bold cterm=bold guifg=yellow")
 vim.cmd("hi LineNrAbove guifg=grey")
 vim.cmd("hi LineNrBelow guifg=grey")
 vim.cmd("hi SignColumn guibg=#1d2021")
@@ -23,31 +42,5 @@ vim.opt.showcmd = true
 vim.opt.cmdheight = 0
 vim.opt.fillchars = { eob = " " }
 vim.loader.enable()
+require("lazy").setup("plugins")
 
-require("lazy").setup({ import = "plugins" }, {
-	install = {
-		missing = true,
-	},
-	checker = {
-		enabled = true,
-		notify = false,
-	},
-	change_detection = {
-		enabled = true,
-		notify = false,
-	},
-	ui = {
-		border = "rounded",
-	},
-	performance = {
-		rtp = {
-			disabled_plugins = {
-				"gzip",
-				"tarPlugin",
-				"tohtml",
-				"tutor",
-				"zipPlugin",
-			},
-		},
-	},
-})
