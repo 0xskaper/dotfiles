59a7b0fdc24f0319f9af025e46bbe28d0ef4b20b
0xskaper <es.rajatyadav@gmail.com>
2025-01-16 15:21:58 -0500

    Update .config/nvim/lua/plugins/gruvbox.lua

--- dot_config/nvim/lua/plugins/gruvbox.lua
+++ dot_config/nvim/lua/plugins/gruvbox.lua
@@ -1,24 +1,26 @@
 return {
-	"ellisonleao/gruvbox.nvim",
-	priority = 1000,
-	opts = ...,
-	config = function()
-		require("gruvbox").setup({
-			contrast = "soft",
-			terminal_colors = true,
-			italic = {
-				strings = false,
-			},
-			overrides = {
-				CursorLineNr = { fg = "#000000" },
-				Keyword = { fg = "#B33F40", bold = true },
-			},
-			palette_overrides = {
-				dark0 = "#1d2021",
-				dark1 = "#1d2021",
-				bright_red = "#B33F40",
-			},
-		})
-		vim.cmd("colorscheme gruvbox")
-	end,
+  "ellisonleao/gruvbox.nvim",
+  priority = 1000,
+  opts = ...,
+  config = function()
+    require("gruvbox").setup({
+      contrast = "soft",
+      terminal_colors = true,
+      italic = {
+        strings = false,
+      },
+      overrides = {
+        CursorLineNr = { fg = "#000000" },
+        Keyword = { fg = "#B33F40", bold = true },
+      },
+      palette_overrides = {
+        dark0 = "#1d2021",
+        dark1 = "#1d2021",
+        bright_red = "#B33F40",
+        light0 = "#ebdbb2",
+        light1 = "#ebdbb2",
+      },
+    })
+    vim.cmd("colorscheme gruvbox")
+  end,
 }
